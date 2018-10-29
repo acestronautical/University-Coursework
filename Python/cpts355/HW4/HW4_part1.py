24389650bf639106b54f8d8e327a112ee016f7a6
Ace Cassidy <ac27182@gmail.com>
2018-10-29 10:52:46 -0700

    clean up formatting

--- Python/cpts355/HW4/HW4_part1.py
+++ Python/cpts355/HW4/HW4_part1.py
@@ -1,7 +1,7 @@
 import re
 
-# ------------------------- 00% ------------------------------------
-# # Utility Functions
+# ------------------------- Utility ------------------------------------
+# # Utility Functions: pop, push
 
 
 def popN(D, N):
@@ -18,8 +18,8 @@ def pushN(D, *items):
     for item in items:
         D.append(item)
 
-# ------------------------- 00% ------------------------------------
-# # Type Checking
+# ------------------------- Type Checking ------------------------------------
+# # Type Check Functions: checkIs- Bool, Integer, Number, List, Dict
 
 
 def checkIsBool(*items):
@@ -51,8 +51,8 @@ def checkIsDict(*items):
         if not isinstance(item, dict):
             raise TypeError("cannot perform operation on non dict type")
 
-# ------------------------- 10% ------------------------------------
-# # The operand stack: define the operand stack and its operations
+# ------------------------- Operand Stack ------------------------------------
+# # Operand Stack Functions: opPop, opPopN, opPush, opPushN
 
 
 opstack = []
@@ -73,9 +73,8 @@ def opPushN(*items):
 def opPush(item):
     opPushN(item)
 
-
- # -------------------------- 20% ------------------------------------
- # # The dictionary stack: define the dictionary stack and its operations
+ # -------------------------- Dictionary Stack ------------------------------------
+ # # Dictionary Stack Functions: dictPop, dictPopN, dictPush, dictPushN , define, lookup
 
 
 dictstack = [{}]
@@ -97,22 +96,11 @@ def dictPush(item):
     dictPushN(item)
 
 
-# dictPush pushes a new dictionary to the dictstack. Note that, your interpreter
-# will call dictPush only when Postscript “begin” operator is called. “begin”
-# should pop the empty dictionary from the opstack and push it onto the dictstack
-# by calling dictPush. You may either pass this dictionary (which you popped from
-# opstack) to dictPush as a parameter or just simply push a new empty dictionary
-# in dictPush.
-
 def define(name, value):
     if isinstance(name, str) and name[0] == "/":
         dictstack[-1][name] = value
     else:
         raise TypeError("name is not a valid string")
-
-# define adds name:value to the top dictionary in the dictionary stack. (Keep the ‘/’ in
-# name when you add it to the top dictionary) Your psDef function should pop the
-# name and value from operand stack and call the “define” function.
 
 
 def lookup(name):
@@ -121,18 +109,9 @@ def lookup(name):
             return dic["/" + name]
     raise ValueError("attempt to access undefined name")
 
- # lookup returns the value associated with name.
- # What is your design decision about what to do when there is no definition for
- # name? If “name” is not defined, your program should not break, but should
- # give an appropriate error message.
 
-
-# --------------------------- 15% ------------------------------------
-# # Arithmetic and comparison operators:
-# define all the arithmetic and comparison operators here
-#  --> add, sub, mul, div, eq, lt, gt
-# Make sure to check the operand stack has the correct number of parameters
-# and types of the parameters are correct.
+# --------------------------- Arithmetic Operators ------------------------------------
+# # Arithmetic Operator Functions: sub, add, div, mul, mod, neg, gt, lt, eq
 
 
 def sub():
@@ -189,8 +168,8 @@ def eq():
     opPush(opand1 == opand2)
 
 
-# --------------------------- 15% ------------------------------------
-# # Array operators: define the array operators length, get
+# --------------------------- Array Operators ------------------------------------
+# # Array Operators Functions: length, get
 
 def length():
     L = opPop()
@@ -205,9 +184,8 @@ def get():
     opPush(opand1[opand2])
 
 
-# --------------------------- 15% ------------------------------------
-# # Boolean operators: define the boolean operators psAnd, psOr, psNot
-# Remember that these take boolean operands only. Anything else is an error
+# --------------------------- Boolean Operators ------------------------------------
+# # Boolean Operator Functions: psAnd, psOr, psNot
 
 
 def psAnd():
@@ -228,9 +206,8 @@ def psNot():
     opPush(not opand1)
 
 
-# --------------------------- 25% ------------------------------------
-# # Define the stack manipulation and print operators:
-# dup, exch, pop, copy, clear, stack
+# --------------------------- Operand Stack Manipulation ------------------------------------
+# # Stack Manipulation and Print Functions: dup, exch, pop, copy, clear, stack
 
 def dup():
     opand1 = opPop()
@@ -263,12 +240,8 @@ def stack():
     for item in opstack:
         print(item)
 
-# --------------------------- 20% ------------------------------------
-# # Define the dictionary manipulation operators: psDict, begin, end, psDef
-# name the function for the def operator psDef because def is reserved in Python.
-# Note: The psDef operator will pop the value and name from the opstack
-# and call your own "define" operator (pass those values as parameters).
-# Note that psDef()won't have any parameters.
+# --------------------------- Dictionary Stack Manipulation ------------------------------------
+# # Dictionary Manipulation Functions: psDict, begin, end, psDef
 
 
 def psDict():
@@ -291,14 +264,6 @@ def psDef():
     value, name = opPopN(2)
     define(name, value)
 
-
-# Important Note: For all operators you need to implement basic checks, i.e., check whether there are
-# sufficient number of values in the operand stack and check whether those values have correct types.
-# Examples:
-# def operator: the operands stack should have 2 values where the second value from top of the stack is
-# a string starting with '/'
-# get operator : the operand stack should have 2 values; the top value on the stack should be an integer
-# and the second value should be an array constant.
 
  # --------------------------- TEST FUNCTIONS ----------------------------------
  # # Include your test functions here
