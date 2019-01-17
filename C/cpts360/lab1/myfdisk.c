f5b80392fca7b0a10d63140d209b5d7fae67ffd3
ace cassidy <ac27182@gmail.com>
2019-01-17 15:13:28 -0800

    fix to allow extended mbr in any slot not just 4

--- C/cpts360/lab1/myfdisk.c
+++ C/cpts360/lab1/myfdisk.c
@@ -5,30 +5,35 @@
 
 void myfdisk(char *file)
 {
-
-    int offset = 446;
-    struct partition *p = 0;
-    int i = 0;
-    printf("Device     Boot Start     End     Sectors    ID\n");
+    char buf[512];
     int fd = open(file, O_RDONLY);
-
-    u8 *buff = malloc(4 * sizeof(struct partition));
+    read(fd, buf, 512);
     if (fd == -1)
-        return;
-    lseek(fd, offset, SEEK_SET);
-    read(fd, buff, 4 * sizeof(struct partition));
-    p = (struct partition *)buff;
-    for (i = 0; i < 4; i++, p++)
-        printf("%s%d           %4d     %4d       %4d   %3x\n", file, i + 1, p->start_sector, p->start_sector + p->nr_sectors - 1, p->nr_sectors, p->sys_type);
-    p--;
-    int p4Start = p->start_sector;
-    while (p->sys_type)
+        printf("Could not open file\n");
+    struct partition *p = (struct partition *)&buf[0x1BE];
+
+    printf("Device     Boot Start     End     Sectors    ID\n");
+    for (int i = 0, pNum = 1; i < 4; i++, p++, pNum++)
     {
-        int numSectors = p4Start + p->start_sector; 
-        lseek(fd, numSectors * 512 + offset, SEEK_SET);
-        read(fd, buff, 2 * sizeof(struct partition));
-        p = (struct partition *)buff;
-        printf("%s%d           %4d     %4d       %4d   %3x\n", file, i + 1, p->start_sector + numSectors, p->start_sector + p->nr_sectors + numSectors - 1, p->nr_sectors, p->sys_type);
-        p++, i++;
+        printf("%s%d           %4d     %4d       %4d   %3x\n", file, pNum, p->start_sector, p->start_sector + p->nr_sectors - 1, p->nr_sectors, p->sys_type);
+
+        if (p->sys_type == 5)
+        {
+            int extStart = p->start_sector;
+            int offset = 0;
+            struct partition *pExt = p;
+            lseek(fd, extStart * 512, SEEK_SET);
+            while (pExt->sys_type)
+            {
+                pNum++;
+                read(fd, buf, 512);
+                pExt = (struct partition *)&buf[0x1BE];
+                int bootStart = pExt->start_sector + offset + extStart;
+                printf("%s%d           %4d     %4d       %4d   %3x\n", file, pNum, bootStart, bootStart + pExt->nr_sectors - 1, pExt->nr_sectors, pExt->sys_type);
+                pExt++;
+                lseek(fd, (extStart + pExt->start_sector) * 512, SEEK_SET);
+                offset = pExt->start_sector;
+            }
+        }
     }
 }
