2a7fc587d8cb90260b5b511c1948be55eada5458
ace cassidy <ac27182@gmail.com>
2019-03-15 21:29:57 -0700

    remove globals conflict

--- C/cpts360/project/src/mount.c
+++
@@ -1,9 +0,0 @@
-#include "global.h"
-#include "type.h"
-#include "util.c"
-
-int ninode, nblocks, bmap, imap, iblock;
-int dev;
-char gline[25], *name[16]; // tokenized component string strings
-int nname;                 // number of component strings
-char *rootdev = "mydisk";  // default root_device
