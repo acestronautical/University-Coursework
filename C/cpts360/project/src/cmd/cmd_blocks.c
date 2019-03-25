f853b73aa8134ca87e1079d912e04099d0deca69
ace cassidy <ac27182@gmail.com>
2019-03-25 01:31:07 -0700

    fix off by one in cmd_blocks

--- C/cpts360/project/src/cmd/cmd_blocks.c
+++ C/cpts360/project/src/cmd/cmd_blocks.c
@@ -15,36 +15,36 @@ bool do_blocks(cmd *c) {
   for (i = 0; i < 12; i++) {
     if (!found_inode->i_block[i])
       break;
-    printf("%d\t", found_inode->i_block[i]);
+    printf("%u\t", found_inode->i_block[i]);
   }
   printf("\n");
 
   printf("indirect blocks:\n");
-  get_block(found_minode->dev, found_inode->i_block[13], buf1);
-  if (found_inode->i_block[13])
-    printf("\n[%d] :\n", found_inode->i_block[13]);
+  get_block(found_minode->dev, found_inode->i_block[12], buf1);
+  if (found_inode->i_block[12])
+    printf("[%u] :\n", found_inode->i_block[12]);
   fs_p1 = (int *)buf1;
   while (*fs_p1)
-    printf("%d\t", *fs_p1++);
+    printf("%u\t", *fs_p1++);
   printf("\n");
 
   printf("double indirect blocks:\n");
-  get_block(found_minode->dev, found_inode->i_block[14], buf1);
-  if (found_inode->i_block[14])
-    printf("\n[%d] :\n", found_inode->i_block[14]);
+  get_block(found_minode->dev, found_inode->i_block[13], buf1);
+  if (found_inode->i_block[13])
+    printf("[%u] :\n", found_inode->i_block[13]);
   fs_p1 = (int *)buf1;
   while (*fs_p1) {
     get_block(found_minode->dev, *fs_p1, buf2);
-    printf("\n[%d] :\n", *fs_p1);
+    printf("[%u] :\n", *fs_p1);
     fs_p2 = (int *)buf2;
     while (*fs_p2)
-      printf("%d\t", *fs_p2++);
+      printf("%u\t", *fs_p2++);
+    printf("\n");
     fs_p1++;
   }
-  // get_block i_block[13]
-  // print through block
+  printf("\n");
 
-  printf("triple indirect blocks:\n");
+  // printf("triple indirect blocks:\n");
 
   put_inode(found_minode);
   return true;
