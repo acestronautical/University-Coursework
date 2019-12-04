e01ebf8a78737ca1e372058dfbc767cf293ef64b
Ace Cassidy <ac27182@gmail.com>
2019-12-04 11:50:57 -0800

    cp started but a little buggy

--- C/cpts460/Final/cp.c
+++ C/cpts460/Final/cp.c
@@ -3,7 +3,7 @@ int main(int argc, char *argv[]) {
   int fd1, fd2;
   char c[1];
   if (argc == 1) {
-    printf("L2U: not enough arguments\n");
+    printf("CP: not enough arguments\n");
     exit(0);
   } else if (argc == 2) {
     fd1 = open(argv[1], O_RDONLY);
@@ -16,19 +16,17 @@ int main(int argc, char *argv[]) {
     printf("Error opening file.\n");
     return 1;
   }
-  char line[1024];
-  while (readline(fd1, line)) {
-    // printf("got line: %s\n", line);
-    for (int i = 0; i < 1024; i++) {
-      if (line[i] > 96 && line[i] < 123)
-        line[i] -= 32;
-    }
-    // printf("line now: %s\n", line);
+  char buf[1024];
+  int n;
+  do {
+    n = read(fd1, buf, 1024);
+    // printf("got: %s", buf);
     if (argc == 2)
-      printf("%s", line);
+      printf("%s", buf);
     else
-      write(fd2, line, 1024);
-  }
+      write(fd2, buf, 1024);
+
+  } while (n == 1024);
   close(fd1);
   close(fd2);
   return 1;
