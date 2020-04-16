12e098b19f23c6f7729a069417ca231e649348e2
Ace Cassidy <ac27182@gmail.com>
2020-04-15 21:39:51 -0700

    finish Q2 of HW3

--- C/cpts483/SampleCode/5.functions.c
+++ C/cpts483/SampleCode/5.functions.c
@@ -13,14 +13,13 @@ int add_3(int a, int b, int c) {
 int main(int argc, char *argv[]) {
   int s = 0;
 
-  for (int i = 0; i < argc; ++i) {
-    s += add_2(i, i);
-    s += add_3(i, i, i);
-  }
+    s = s + add_2(2, 3);
+    s = s + add_3(4, 5, 6);
+  
 
   return 0;
 }
 
-// clang -O0 -Xclang -disable-O0-optnone -S -emit-llvm $1  -o test.ll && \
-// opt -load ~/repos/llvm-project/build/lib/LLVMHW3.so -HW -S test.ll  -o dev.ll && \
+// clang -O0 -Xclang -disable-O0-optnone -S -emit-llvm 5.functions.c  -o test.ll
+// opt -load ~/repos/llvm-project/build/lib/LLVMHW3.so -HW -S test.ll  -o dev.ll
 // clang dev.ll
